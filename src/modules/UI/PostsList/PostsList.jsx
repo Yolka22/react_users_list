7cf35ee916467024573b85442a2eddb53db6baea
Yehor <egorkorsun7@gmail.com>
2023-08-09 00:55:30 +0200

    litl animations added

--- src/modules/UI/PostsList/PostsList.jsx
+++ src/modules/UI/PostsList/PostsList.jsx
@@ -1,14 +1,26 @@
-import React from 'react'
-import Post from '../Post/Post'
-import "./PostsList"
-const PostsList = ({postsArray}) =>{
-    return(
-        <div className='posts'>
-            {postsArray.map((post) => (
+import React, { useState } from 'react';
+import Post from '../Post/Post';
+import './PostsList.css';
+
+const PostsList = ({ postsArray }) => {
+  const [visiblePosts, setVisiblePosts] = useState(5);
+
+  const loadMorePosts = () => {
+    setVisiblePosts(prevVisiblePosts => prevVisiblePosts + 5);
+  };
+
+  return (
+    <div className="posts">
+      {postsArray.slice(0, visiblePosts).map(post => (
         <Post key={post.id} postData={post} />
       ))}
-        </div>
-    )
-}
+      {visiblePosts < postsArray.length && (
+        <button  onClick={loadMorePosts} className="custom-btn">
+          Load More
+        </button>
+      )}
+    </div>
+  );
+};
 
-export default PostsList
+export default PostsList;
